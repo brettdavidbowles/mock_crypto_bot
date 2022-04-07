73f1ab3e9b9323b5a5e1fa4986a3076c2f76f3e1
Brett Bowles <brettbowles@Bretts-MacBook-Pro.local>
2022-04-06 23:50:02 -0400

    shit works, just set up cron job in heroku

--- mock_data_script.py
+++ mock_data_script.py
@@ -1,103 +1,114 @@
 from graphqlclient import GraphQLClient
 gqlClient = GraphQLClient('https://crypto-bots-back.herokuapp.com/graphql')
 import random
-import json
-import csv
-from six.moves import urllib
 import requests
+from datetime import datetime
+import json
 
-from binance.client import Client
 
-API_Key='4a2L2JdIsUGshgaCBZF56bhdTMWikgdOgQtgzkawtr7MfyM4rNfXOHztHEEzlz0S' 
-Secret_Key='5d2C7XDQ7kwC51WjycVZZssr0eB8PQW5XfYzGdV9ndyxTF6ZqdoWnknnwl199D3o'
-client = Client(API_Key, Secret_Key, tld='us')
-client.API_URL='https://testnet.binance.vision/api'
+def coin_price(coin):
+  url = 'https://api.binance.com/api/v3/avgPrice?symbol=' + coin
+  price = requests.get(url).json()['price']
+  # data_dict = json.loads(data)
+  return price
 
-def get_data():
-  url = 'https://api.binance.com/api/v3/avgPrice?symbol=BNBBTC'
-  data = requests.get(url).json()
-  print(data)
+query = """
+    mutation transactionCreate(
+        $bot_name:String!,
+        $coin_abbrev:String!,
+        $username:String!,
+        $api_key:String!,
+        $is_sale:Boolean!,
+        $coin_quantity:Decimal!,
+        $transaction_price:Decimal!,
+        $contemporary_coin_price:Decimal!,
+        $profit:Decimal!,
+        $transaction_name:String!
+        ){
+        createTransaction(
+            inputData: {
+                bot: {
+                    name: $bot_name
+                },
+                coin:{
+                    abbrev: $coin_abbrev
+                }
+                user: {
+                    username: $username
+                    apiKey: $api_key
+                }
+                isSale: $is_sale
+                coinQuantity: $coin_quantity
+                transactionPrice: $transaction_price
+                contemporaryCoinPrice: $contemporary_coin_price
+                profit: $profit
+                name: $transaction_name
+            }
+        ){
+            transaction{
+                bot{
+                    name
+                    id
+                }
+                coin{
+                    name
+                    abbrev
+                }
+                user{
+                    username
+                }
+                coinQuantity
+                transactionPrice
+                contemporaryCoinPrice
+                transactionDateTime
+                profit
+                name
+                changeInTotal
+            }
+        }
+    }"""
 
-get_data()
-# assets_url = "https://data.messari.io/api/v1/assets"
-# assets_bytes = urllib.request.urlopen(assets_url).read()
-# assets_json = assets_bytes.decode('utf8').replace("'", '"')
-# assets_dict = json.loads(assets_json)
+def send_mock_data(variables):
+  gqlClient.execute(query, variables)
 
-# ticker_list = []
-# for asset in assets_dict["data"]:
-#     if (asset["symbol"] != "btc") and (asset["symbol"] != "eth") and (asset["symbol"] != "ltc"):
-#         pass
-#     else:
-#         ticker_list.append(asset["symbol"])
-#         print(ticker_list)
+coins = [ 'BTCUSDT', 'ETHUSDT', 'LTCUSDT' ]
 
-# query = """
-#     mutation transactionCreate(
-#         $bot_name:String!,
-#         $coin_abbrev:String!,
-#         $username:String!,
-#         $api_key:String!,
-#         $is_sale:Boolean!,
-#         $coin_quantity:Decimal!,
-#         $contemporary_coin_price:Decimal!,
-#         $profit:Decimal!,
-#         $transaction_name:String!
-#         ){
-#         createTransaction(
-#             inputData: {
-#                 bot: {
-#                     name: $bot_name
-#                 },
-#                 coin:{
-#                     abbrev: $coin_abbrev
-#                 }
-#                 user: {
-#                     username: $username
-#                     apiKey: $api_key
-#                 }
-#                 isSale: $is_sale
-#                 coinQuantity: $coin_quantity
-#                 contemporaryCoinPrice: $contemporary_coin_price
-#                 profit: $profit
-#                 name: $transaction_name
-#             }
-#         ){
-#             transaction{
-#                 bot{
-#                     name
-#                     id
-#                 }
-#                 coin{
-#                     name
-#                     abbrev
-#                 }
-#                 user{
-#                     username
-#                 }
-#                 coinQuantity
-#                 contemporaryCoinPrice
-#                 transactionDateTime
-#                 profit
-#                 name
-#                 changeInTotal
-#             }
-#         }
-#     }"""
+for coin in coins:
+  transaction_price_deviation_from_contemporary_price = random.random()
+  price = float(coin_price(coin))
+  quanity = random.random()
+  transactionPrice = price - transaction_price_deviation_from_contemporary_price
+  send_mock_data(
+    {
+      'bot_name': 'mockbot',
+      'coin_abbrev': coin,
+      'username': 'mockuser',
+      'api_key': 'cfc1e3f4-5d07-49fc-9bbf-05ceeeffe626',
+      'is_sale': False,
+      'coin_quantity': quanity,
+      'transaction_price': transactionPrice,
+      'contemporary_coin_price': price,
+      'profit': ( quanity * price ) - ( quanity * transactionPrice ),
+      'transaction_name': 'mock BTCUSDT' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    }
+  )
 
-# def send_mock_data(variables):
-#   gqlClient.execute(query, variables)
-
-# def random_reasonable_coin_price(coin):
-#   if coin
-# send_mock_data({
-#   'bot_name': 'mockbot',
-#   'coin_abbrev': 'BTCUSD',
-#   'username': 'mockuser',
-#   'api_key': 'cfc1e3f4-5d07-49fc-9bbf-05ceeeffe626',
-#   'is_sale': True,
-#   'coin_quantity': random.random(),
-#   'contemporary_coin_price': 2,
-#   'profit': 2,
-#   'transaction_name': 'testtransaction'
-# })
+for coin in coins:
+  transaction_price_deviation_from_contemporary_price = random.random()
+  price = float(coin_price(coin))
+  quanity = random.random()
+  transactionPrice = price + transaction_price_deviation_from_contemporary_price
+  send_mock_data(
+    {
+      'bot_name': 'mockbot',
+      'coin_abbrev': coin,
+      'username': 'mockuser',
+      'api_key': 'cfc1e3f4-5d07-49fc-9bbf-05ceeeffe626',
+      'is_sale': True,
+      'coin_quantity': quanity,
+      'transaction_price': transactionPrice,
+      'contemporary_coin_price': price,
+      'profit': ( quanity * transactionPrice ) - ( quanity * price ),
+      'transaction_name': 'mock BTCUSDT' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    }
+  )
